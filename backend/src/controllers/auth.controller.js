70825822976afe028f2cdb19fd87f9b56098e3af
Aquinnos <117024828+Aquinnos@users.noreply.github.com>
2025-02-15 16:47:40 +0100

    add initial project structure with configuration files and module exports

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = async (req, res) => {
+    res.send('Sign-up');
+};
+
+export const signin = async (req, res) => {
+    res.send('Sign-in');
+}
+
+export const signout = async (req, res) => {
+    res.send('Sign-out');
+}
+
